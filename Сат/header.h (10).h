8102c512885db978729768e47711df4e48975ab0
Чойган-4409 <ffaer9109@gmail.com>
2026-02-13 21:35:11 +0700

    Delete Сат/header.h (10).h

--- Сат/header.h (10).h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_H_INCLUDED
-#define HEADER_H_INCLUDED
-
-void enter_dannih(double &x, double &y);
-double raschet(double x, double y);
-void vivod_dannih(double z);
-
-#endif // HEADER_H_INCLUDED
